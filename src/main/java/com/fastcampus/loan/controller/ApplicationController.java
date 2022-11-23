486d527fa6a6f8c9dcbfaf77282eab5f576e2455
Jongheon Kim <sapzape@gmail.com>
2022-11-23 14:41:18 +0900

    [CH-03] 대출 신청 등록 기능 구현 (#9)

---
+++ src/main/java/com/fastcampus/loan/controller/ApplicationController.java
@@ -0,0 +1,24 @@
+package com.fastcampus.loan.controller;
+
+import com.fastcampus.loan.dto.ApplicationDTO.Request;
+import com.fastcampus.loan.dto.ApplicationDTO.Response;
+import com.fastcampus.loan.dto.ResponseDTO;
+import com.fastcampus.loan.service.ApplicationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/applications")
+public class ApplicationController extends AbstractController {
+
+  private final ApplicationService applicationService;
+
+  @PostMapping
+  public ResponseDTO<Response> create(@RequestBody Request request) {
+    return ok(applicationService.create(request));
+  }
+}
---
+++ src/main/java/com/fastcampus/loan/service/ApplicationService.java
@@ -0,0 +1,9 @@
+package com.fastcampus.loan.service;
+
+import com.fastcampus.loan.dto.ApplicationDTO.Request;
+import com.fastcampus.loan.dto.ApplicationDTO.Response;
+
+public interface ApplicationService {
+
+  Response create(Request request);
+}
