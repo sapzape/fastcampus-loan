a4812137d0a785cca14ddc68ff68c1fda1c3f98a
Jongheon Kim <sapzape@gmail.com>
2022-12-14 17:54:17 +0900

    [CH-03] 대출 약관 동의 기능 구현

--- src/main/java/com/fastcampus/loan/service/ApplicationServiceImpl.java
+++ src/main/java/com/fastcampus/loan/service/ApplicationServiceImpl.java
@@ -1,14 +1,24 @@
 package com.fastcampus.loan.service;
 
+import com.fastcampus.loan.domain.AcceptTerms;
 import com.fastcampus.loan.domain.Application;
+import com.fastcampus.loan.domain.Terms;
+import com.fastcampus.loan.dto.ApplicationDTO;
 import com.fastcampus.loan.dto.ApplicationDTO.Request;
 import com.fastcampus.loan.dto.ApplicationDTO.Response;
 import com.fastcampus.loan.exception.BaseException;
 import com.fastcampus.loan.exception.ResultType;
+import com.fastcampus.loan.repository.AcceptTermsRepository;
 import com.fastcampus.loan.repository.ApplicationRepository;
+import com.fastcampus.loan.repository.TermsRepository;
 import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,6 +26,10 @@ import org.springframework.stereotype.Service;
 public class ApplicationServiceImpl implements ApplicationService {
 
   private final ApplicationRepository applicationRepository;
+
+  private final TermsRepository termsRepository;
+
+  private final AcceptTermsRepository acceptTermsRepository;
 
   private final ModelMapper modelMapper;
 
@@ -63,5 +77,40 @@ public class ApplicationServiceImpl implements ApplicationService {
     application.setIsDeleted(true);
 
     applicationRepository.save(application);
+  }
+
+  @Override
+  public Boolean acceptTerms(Long applicationId, ApplicationDTO.AcceptTerms dto) {
+    applicationRepository.findById(applicationId).orElseThrow(() -> {
+      throw new BaseException(ResultType.SYSTEM_ERROR);
+    });
+
+    List<Terms> termsList = termsRepository.findAll(Sort.by(Direction.ASC, "termsId"));
+    if (termsList.isEmpty()) {
+      throw new BaseException(ResultType.SYSTEM_ERROR);
+    }
+
+    List<Long> acceptTermsIds = dto.getAcceptTermsIds();
+    if (termsList.size() != acceptTermsIds.size()) {
+      throw new BaseException(ResultType.SYSTEM_ERROR);
+    }
+
+    List<Long> termsIds = termsList.stream().map(Terms::getTermsId).collect(Collectors.toList());
+    Collections.sort(acceptTermsIds);
+
+    if (!termsIds.containsAll(acceptTermsIds)) {
+      throw new BaseException(ResultType.SYSTEM_ERROR);
+    }
+
+    for (Long termsId : acceptTermsIds) {
+      AcceptTerms accepted = AcceptTerms.builder()
+          .termsId(termsId)
+          .applicationId(applicationId)
+          .build();
+
+      acceptTermsRepository.save(accepted);
+    }
+
+    return true;
   }
 }
--- src/test/java/com/fastcampus/loan/service/ApplicationServiceTest.java
+++ src/test/java/com/fastcampus/loan/service/ApplicationServiceTest.java
@@ -3,12 +3,21 @@ package com.fastcampus.loan.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
+import com.fastcampus.loan.domain.AcceptTerms;
 import com.fastcampus.loan.domain.Application;
+import com.fastcampus.loan.domain.Terms;
+import com.fastcampus.loan.dto.ApplicationDTO;
 import com.fastcampus.loan.dto.ApplicationDTO.Request;
 import com.fastcampus.loan.dto.ApplicationDTO.Response;
+import com.fastcampus.loan.exception.BaseException;
+import com.fastcampus.loan.repository.AcceptTermsRepository;
 import com.fastcampus.loan.repository.ApplicationRepository;
+import com.fastcampus.loan.repository.TermsRepository;
 import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatchers;
@@ -17,6 +26,8 @@ import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 
 @ExtendWith(MockitoExtension.class)
 public class ApplicationServiceTest {
@@ -26,6 +37,12 @@ public class ApplicationServiceTest {
 
   @Mock
   private ApplicationRepository applicationRepository;
+
+  @Mock
+  private TermsRepository termsRepository;
+
+  @Mock
+  private AcceptTermsRepository acceptTermsRepository;
 
   @Spy
   private ModelMapper modelMapper;
@@ -104,5 +121,95 @@ public class ApplicationServiceTest {
     applicationService.delete(targetId);
 
     assertThat(entity.getIsDeleted()).isSameAs(true);
+  }
+
+  @Test
+  void Should_AddAcceptTerms_When_RequestAcceptTermsOfApplication() {
+    Terms entityA = Terms.builder()
+        .termsId(1L)
+        .name("대출 이용 약관 1")
+        .termsDetailUrl("https://abc-storage.acc/dslfjdlsfjlsdddads")
+        .build();
+
+    Terms entityB = Terms.builder()
+        .termsId(2L)
+        .name("대출 이용 약관 2")
+        .termsDetailUrl("https://abc-storage.acc/dslfjdlsfjlsdweqwq")
+        .build();
+
+    List<Long> acceptTerms = Arrays.asList(1L, 2L);
+
+    ApplicationDTO.AcceptTerms request = ApplicationDTO.AcceptTerms.builder()
+        .acceptTermsIds(acceptTerms)
+        .build();
+
+    Long findId = 1L;
+
+    when(applicationRepository.findById(findId)).thenReturn(
+        Optional.ofNullable(Application.builder().build()));
+    when(termsRepository.findAll(Sort.by(Direction.ASC, "termsId"))).thenReturn(Arrays.asList(entityA, entityB));
+    when(acceptTermsRepository.save(ArgumentMatchers.any(AcceptTerms.class))).thenReturn(AcceptTerms.builder().build());
+
+
+    Boolean actual = applicationService.acceptTerms(findId, request);
+    assertThat(actual).isTrue();
+  }
+
+  @Test
+  void Should_ThrowException_When_RequestNotAllAcceptTermsOfApplication() {
+    Terms entityA = Terms.builder()
+        .termsId(1L)
+        .name("대출 이용 약관 1")
+        .termsDetailUrl("https://abc-storage.acc/dslfjdlsfjlsdddads")
+        .build();
+
+    Terms entityB = Terms.builder()
+        .termsId(2L)
+        .name("대출 이용 약관 2")
+        .termsDetailUrl("https://abc-storage.acc/dslfjdlsfjlsdweqwq")
+        .build();
+
+    List<Long> acceptTerms = Arrays.asList(1L);
+
+    ApplicationDTO.AcceptTerms request = ApplicationDTO.AcceptTerms.builder()
+        .acceptTermsIds(acceptTerms)
+        .build();
+
+    Long findId = 1L;
+
+    when(applicationRepository.findById(findId)).thenReturn(
+        Optional.ofNullable(Application.builder().build()));
+    when(termsRepository.findAll(Sort.by(Direction.ASC, "termsId"))).thenReturn(Arrays.asList(entityA, entityB));
+
+    Assertions.assertThrows(BaseException.class, () -> applicationService.acceptTerms(1L, request));
+  }
+
+  @Test
+  void Should_ThrowException_When_RequestNotExistAcceptTermsOfApplication() {
+    Terms entityA = Terms.builder()
+        .termsId(1L)
+        .name("대출 이용 약관 1")
+        .termsDetailUrl("https://abc-storage.acc/dslfjdlsfjlsdddads")
+        .build();
+
+    Terms entityB = Terms.builder()
+        .termsId(2L)
+        .name("대출 이용 약관 2")
+        .termsDetailUrl("https://abc-storage.acc/dslfjdlsfjlsdweqwq")
+        .build();
+
+    List<Long> acceptTerms = Arrays.asList(1L, 3L);
+
+    ApplicationDTO.AcceptTerms request = ApplicationDTO.AcceptTerms.builder()
+        .acceptTermsIds(acceptTerms)
+        .build();
+
+    Long findId = 1L;
+
+    when(applicationRepository.findById(findId)).thenReturn(
+        Optional.ofNullable(Application.builder().build()));
+    when(termsRepository.findAll(Sort.by(Direction.ASC, "termsId"))).thenReturn(Arrays.asList(entityA, entityB));
+
+    Assertions.assertThrows(BaseException.class, () -> applicationService.acceptTerms(1L, request));
   }
 }
