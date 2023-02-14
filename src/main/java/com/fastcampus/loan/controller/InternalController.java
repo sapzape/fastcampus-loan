cc42b6c8cc8fa6087c3e991a1e70247f8d57ccdd
Jongheon Kim <sapzape@gmail.com>
2023-02-15 07:30:45 +0900

    Update internal api path

--- src/main/java/com/fastcampus/loan/controller/InternalController.java
+++ src/main/java/com/fastcampus/loan/controller/InternalController.java
@@ -32,14 +32,14 @@ public class InternalController extends AbstractController {
     return ok(entryService.update(applicationId, request));
   }
 
-  @GetMapping("/{applicationId}/entries")
-  public ResponseDTO<Response> get(@PathVariable Long applicationId) {
-    return ok(entryService.get(applicationId));
+  @GetMapping("/entries/{entryId}")
+  public ResponseDTO<Response> get(@PathVariable Long entryId) {
+    return ok(entryService.get(entryId));
   }
 
-  @DeleteMapping("/{applicationId}/entries")
-  public ResponseDTO<Void> delete(@PathVariable Long applicationId) {
-    entryService.delete(applicationId);
+  @DeleteMapping("/entries/{entryId}")
+  public ResponseDTO<Void> delete(@PathVariable Long entryId) {
+    entryService.delete(entryId);
     return ok();
   }
 }
